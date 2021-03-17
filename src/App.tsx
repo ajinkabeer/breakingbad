0ef2d9149986e86bf46375f44a6b49c1a7e6b297
Ajin Kabeer <ajiinkabeer@gmail.com>
2021-03-17 18:18:24 +0100

    Fetch and render character response in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
-const App = () => (
-  <div>
-    <header>
-      <h1>Breaking Bad</h1>
-    </header>
-  </div>
-);
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  fetchCharactersThunk,
+  charactersSelector,
+} from './store/slices/characters';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCharactersThunk());
+  }, [dispatch]);
+
+  const { response, loading } = useSelector(charactersSelector);
+
+  if (loading) {
+    return <div>Loading..</div>;
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Breaking Bad</h1>
+        <code>{JSON.stringify(response)}</code>
+      </header>
+    </div>
+  );
+};
 
 export default App;
